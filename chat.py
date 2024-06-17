43e6156fe73c0f4dc862009656aa006eea4fd3fe
saneness <saneness@protonmail.com>
2024-06-17 10:54:51 +0000

    add history support to chats

--- chat.py
+++ chat.py
@@ -2,12 +2,15 @@
 
 # -*- coding: utf-8 -*-
 
-import os
 import argparse
+import json
+import os
 from openai import OpenAI
 
 def args():
     parser   = argparse.ArgumentParser(description="Wrapper to generate OpenAI messages.")
+    parser.add_argument("-u", "--user", metavar="\b", required=True, help="user to get conversation history for")
+    parser.add_argument("-l", "--log",  metavar="\b", default="/tmp", help="conversation logs")
     parser.add_argument("-m", "--model", metavar="\b", default="gpt-3.5-turbo", help="model to use (default: gpt-3.5-turbo)")
     parser.add_argument("-p", "--prompt", metavar="\b", required=True, help="Prompt to generate response from")
     args = parser.parse_args()
@@ -15,8 +18,17 @@ def args():
 
 if __name__ == '__main__':
     args = args()
-    client = OpenAI()
-    response = client.chat.completions.create(model=args.model, messages=[
+    messages = []
+    history = f"{args.log}/{args.user}"
+    if os.path.isfile(history):
+        messages = json.loads(open(history).read().strip())
+    messages.append(
         {"role": "user", "content": f"{args.prompt}"}
-    ])
-    print(f"{response.choices[0].message.content}")
+    )
+    client = OpenAI()
+    response = client.chat.completions.create(model=args.model, messages=messages).choices[0].message.content
+    messages.append(
+        {"role": "assistant", "content": f"{response}"}
+    )
+    open(history, "w+").write(json.dumps(messages))
+    print(f"{response}")
