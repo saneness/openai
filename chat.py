3aa2732a9b8dc36bf0757ce04d6d15018374b1f8
saneness <saneness@protonmail.com>
2024-06-17 13:43:07 +0000

    add autoremoval of history

--- chat.py
+++ chat.py
@@ -3,32 +3,51 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import datetime
 import json
+import math
 import os
 from openai import OpenAI
 
 def args():
     parser   = argparse.ArgumentParser(description="Wrapper to generate OpenAI messages.")
-    parser.add_argument("-u", "--user", metavar="\b", required=True, help="user to get conversation history for")
+    parser.add_argument("-u", "--user", metavar="\b", help="user to get conversation history for")
     parser.add_argument("-l", "--log",  metavar="\b", default="/tmp", help="conversation logs")
     parser.add_argument("-m", "--model", metavar="\b", default="gpt-3.5-turbo", help="model to use (default: gpt-3.5-turbo)")
-    parser.add_argument("-p", "--prompt", metavar="\b", required=True, help="Prompt to generate response from")
+    parser.add_argument("-p", "--prompt", metavar="\b", help="Prompt to generate response from")
+    parser.add_argument("-c", "--clean", action="store_true", help="Run clean checks for all the chats")
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
     args = args()
-    messages = []
-    history = f"{args.log}/{args.user}"
-    if os.path.isfile(history):
-        messages = json.loads(open(history).read().strip())
-    messages.append(
-        {"role": "user", "content": f"{args.prompt}"}
-    )
-    client = OpenAI()
-    response = client.chat.completions.create(model=args.model, messages=messages).choices[0].message.content
-    messages.append(
-        {"role": "assistant", "content": f"{response}"}
-    )
-    open(history, "w+").write(json.dumps(messages))
-    print(f"{response}")
+    if not args.clean:
+        messages = []
+        history = f"{args.log}/{args.user}"
+        if os.path.isfile(history):
+            messages = json.loads(open(history).read().strip())
+        messages.append(
+            {"role": "user", "content": f"{args.prompt}"}
+        )
+        client = OpenAI()
+        response = client.chat.completions.create(model=args.model, messages=messages).choices[0].message.content
+        messages.append(
+            {"role": "assistant", "content": f"{response}"}
+        )
+        open(history, "w+").write(json.dumps(messages))
+        print(f"{response}")
+    else:
+        X1 = 40
+        Y1 =  1
+        X2 =  4
+        Y2 = 10
+        W  = -0.1
+        history = f"{args.log}"
+        for user in os.listdir(history):
+            log = os.path.join(history, user)
+            if os.path.isfile(log):
+                size = os.path.getsize(log) / 1024
+                time = (Y2-Y1)*(math.exp(W*size)-math.exp(W*X1))/(math.exp(W*X2)-math.exp(W*X1)) + Y1
+                mtime = (datetime.datetime.now().timestamp() - os.path.getmtime(log)) / 3600
+                if mtime > time:
+                    os.remove(log)
