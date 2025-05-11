d4c1ac7bd963857c3b5fcf5f4c41c6ae2df36575
saneness <saneness@protonmail.com>
2025-05-11 13:27:56 +0100

    add history clear support

--- chat.py
+++ chat.py
@@ -13,15 +13,16 @@ def args():
     parser   = argparse.ArgumentParser(description="Wrapper to generate OpenAI messages.")
     parser.add_argument("-u", "--user", metavar="\b", help="user to get conversation history for")
     parser.add_argument("-l", "--log",  metavar="\b", default="/tmp", help="conversation logs")
-    parser.add_argument("-m", "--model", metavar="\b", default="gpt-4o-mini", help="model to use (default: gpt-4o-mini)")
+    parser.add_argument("-m", "--model", metavar="\b", default="gpt-4.1-nano", help="model to use (default: gpt-4.1-nano)")
     parser.add_argument("-p", "--prompt", metavar="\b", help="Prompt to generate response from")
-    parser.add_argument("-c", "--clean", action="store_true", help="Run clean checks for all the chats")
+    parser.add_argument("-c", "--clear", action="store_true", help="Clears chat history")
+    parser.add_argument("-ca", "--clearall", action="store_true", help="Clears history for all the chats")
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
     args = args()
-    if not args.clean:
+    if not args.clear and not args.clearall:
         messages = []
         history = f"{args.log}/{args.user}"
         if os.path.isfile(history):
@@ -39,17 +40,7 @@ if __name__ == '__main__':
     else:
         history = f"{args.log}"
         for user in os.listdir(history):
-            log = os.path.join(history, user)
-            if os.path.isfile(log):
-                size = os.path.getsize(log) / 1024
-                # exponential formula for storage time
-                # X1 = 40
-                # Y1 =  1
-                # X2 =  4
-                # Y2 = 10
-                # W  = -0.1
-                # time = (Y2-Y1)*(math.exp(W*size)-math.exp(W*X1))/(math.exp(W*X2)-math.exp(W*X1)) + Y1 
-                time = 1
-                mtime = (datetime.datetime.now().timestamp() - os.path.getmtime(log)) / 3600
-                if mtime > time:
+            if args.clearall or args.clear and user == args.user:
+                log = os.path.join(history, user)
+                if os.path.isfile(log):
                     os.remove(log)
